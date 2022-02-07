3ae9dcdeb8755f51ceb2bec4c75d3ca27c99abe9
Mark Wylde <markwylde@users.noreply.github.com>
2022-02-07 14:44:53 +0000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,4 +6,5 @@ import App from './pages/App';
 document.addEventListener('DOMContentLoaded', function () {
   const container = document.body.children[0];
   ReactDOM.render(<App />, container);
+  document.title = 'ReactFast';
 });
