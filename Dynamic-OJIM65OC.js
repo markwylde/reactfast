bb500fd956fef2e74d56b0d83746a0918572b7ce
markwylde <markwylde@users.noreply.github.com>
2022-02-16 19:34:20 +0000

    Deploying to gh-pages from @ markwylde/reactfast@fdd0ee2228117b63256c2f68bba5aadec9ae0017 🚀

---
+++ Dynamic-OJIM65OC.js
@@ -0,0 +1,2 @@
+import{a as i}from"./chunk-LLSNI6KO.js";import{b as t,d as m}from"./chunk-Z7FM7WIK.js";function l(){let[e,o]=i(["first item"]),n=()=>{o([...e,"item "+(e.length+1)])};return t("div",null,t(m,null),t("h1",null,"Does it handle interactivity?"),t("div",null,"Items:"),t("ul",null,e.map((d,r)=>t("li",{key:r},d))),t("button",{onClick:n},"Add Item"))}var c=l;export{c as default};
+//# sourceMappingURL=Dynamic-OJIM65OC.js.map
