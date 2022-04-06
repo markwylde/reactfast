857d7e237366a60502c2ae99a6110b26c20cd85d
markwylde <markwylde@users.noreply.github.com>
2022-04-06 20:24:52 +0000

    Deploying to gh-pages from @ markwylde/reactfast@526fa35d4f6fdeb022675c7c5d0a6fa2736bfa84 🚀

---
+++ 526fa35d4f6fdeb022675c7c5d0a6fa2736bfa84/SEO-MVNSVMS6.js
@@ -0,0 +1,2 @@
+import{b as t,d as e}from"./chunk-Z7FM7WIK.js";function o(){return t("div",null,t(e,null),t("h1",null,"How does SEO work?"),t("p",null,"Yeah, SEO is fine with this app. But you need to make sure you use a host/cdn that is capabile of always returning an index.html page with 200 status."))}var n=o;export{o as SEO,n as default};
+//# sourceMappingURL=SEO-MVNSVMS6.js.map
