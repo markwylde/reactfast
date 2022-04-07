3ce97e871488ea2dcf27dfabbcd241be931ea543
markwylde <markwylde@users.noreply.github.com>
2022-04-07 20:00:17 +0000

    Deploying to gh-pages from @ markwylde/reactfast@8a6351c5d06cfb1b376f346e204dad8123c47069 🚀

---
+++ Home-7Y577AJN.js
@@ -0,0 +1,2 @@
+import{b as e,d as t}from"./chunk-J33IWYRO.js";function o(){return e("div",null,e(t,null),e("h1",null,"What's this all about then my?"),e("p",null,"This is an example of a small, lightweight, fast React project that doesn't need to be server rendered."))}var n=o;export{o as HomePage,n as default};
+//# sourceMappingURL=Home-7Y577AJN.js.map
