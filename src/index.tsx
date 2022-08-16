c7e7076ff514a6b08491c0b6607fc9f9bf93c049
Mark Wylde <me@markwylde.com>
2022-08-16 14:05:57 +0100

    DOMContentLoaded is no longer needed

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import { h, Component, render } from 'preact';
 
 import App from './App';
 
-document.addEventListener('DOMContentLoaded', function () {
-  const container = document.body.children[0];
-  render(<App />, container);
-  document.title = 'ReactFast';
-});
+const container = document.body.children[0];
+render(<App />, container);
+document.title = 'ReactFast';
