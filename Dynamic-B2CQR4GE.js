ab45241a798b089ac341f930e0e63aad021ad663
markwylde <markwylde@users.noreply.github.com>
2023-01-25 07:28:38 +0000

    Deploying to gh-pages from @ markwylde/reactfast@e049976bfe134bd4a16a4b107b569a1c5b711953 🚀

---
+++ Dynamic-B2CQR4GE.js
@@ -0,0 +1,2 @@
+import{a as i}from"./chunk-5MNU4ZOP.js";import{b as t,d as m}from"./chunk-SNB7XQHJ.js";function l(){let[e,o]=i(["first item"]),n=()=>{o([...e,"item "+(e.length+1)])};return t("div",null,t(m,null),t("h1",null,"Does it handle interactivity?"),t("div",null,"Items:"),t("ul",null,e.map((d,r)=>t("li",{key:r},d))),t("button",{onClick:n},"Add Item"))}var c=l;export{c as default};
+//# sourceMappingURL=Dynamic-B2CQR4GE.js.map
---
+++ SEO-X5KGZE5P.js
@@ -0,0 +1,2 @@
+import{b as t,d as e}from"./chunk-SNB7XQHJ.js";function o(){return t("div",null,t(e,null),t("h1",null,"How does SEO work?"),t("p",null,"Yeah, SEO is fine with this app. But you need to make sure you use a host/cdn that is capabile of always returning an index.html page with 200 status."))}var n=o;export{o as SEO,n as default};
+//# sourceMappingURL=SEO-X5KGZE5P.js.map
